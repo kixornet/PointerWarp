fa508b76a198737561b03edd33e68bc8e4ca1818
Micah Taylor <micah@kixor.net>
2020-06-16 07:48:51 -0400

    Add controls to form.

--- PointerWarp/Form1.Designer.cs
+++ PointerWarp/Form1.Designer.cs
@@ -28,12 +28,97 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.button_warpToggle = new System.Windows.Forms.Button();
+			this.textBox_warpRange = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.checkBox_onTop = new System.Windows.Forms.CheckBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button_warpToggle
+			// 
+			this.button_warpToggle.Location = new System.Drawing.Point(6, 24);
+			this.button_warpToggle.Name = "button_warpToggle";
+			this.button_warpToggle.Size = new System.Drawing.Size(58, 23);
+			this.button_warpToggle.TabIndex = 0;
+			this.button_warpToggle.Text = "Engage";
+			this.button_warpToggle.UseVisualStyleBackColor = true;
+			// 
+			// textBox_warpRange
+			// 
+			this.textBox_warpRange.Location = new System.Drawing.Point(168, 24);
+			this.textBox_warpRange.Name = "textBox_warpRange";
+			this.textBox_warpRange.Size = new System.Drawing.Size(54, 20);
+			this.textBox_warpRange.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F);
+			this.label1.Location = new System.Drawing.Point(3, 5);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(136, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Toggle: Shift+Backspace x2";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(165, 8);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(63, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Warp range";
+			// 
+			// checkBox_onTop
+			// 
+			this.checkBox_onTop.AutoSize = true;
+			this.checkBox_onTop.Checked = true;
+			this.checkBox_onTop.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox_onTop.Location = new System.Drawing.Point(70, 27);
+			this.checkBox_onTop.Name = "checkBox_onTop";
+			this.checkBox_onTop.Size = new System.Drawing.Size(92, 17);
+			this.checkBox_onTop.TabIndex = 1;
+			this.checkBox_onTop.Text = "Always on top";
+			this.checkBox_onTop.UseVisualStyleBackColor = true;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F);
+			this.label3.Location = new System.Drawing.Point(223, 26);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(16, 13);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "%";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(245, 52);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.checkBox_onTop);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox_warpRange);
+			this.Controls.Add(this.button_warpToggle);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "Form1";
+			this.Text = "PointerWarp";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+		private System.Windows.Forms.TextBox textBox_warpRange;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.CheckBox checkBox_onTop;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button_warpToggle;
 	}
 }
 
